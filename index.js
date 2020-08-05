ac7535e71ad7af2be3e37932b9eaf890fd57fe66
Greg Doyle <caligd45@gmail.com>
2020-08-05 12:58:10 -0600

    added gitignore and final code to index

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
   {
@@ -45,11 +48,16 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return false.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then((inquirerResponses) => {
+        console.log("Generating README...");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+    })
 }
 
 // function call to initialize program
